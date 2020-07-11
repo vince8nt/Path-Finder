39092d2389ceaadbfbf3da496c46287ee9e90900
vtittert <vtittert@ucsc.edu>
2020-07-10 23:52:04 -0700

    adding files

---
+++ js/pathFinder.js
@@ -0,0 +1,34 @@
+class Square extends React.Component {
+    render() {
+        return (
+            /*
+            <button className = "square">
+                
+            </button>
+            */
+            React.createElement("button");
+        );
+    }
+}
+
+class Map extends React.Component {
+    render() {
+
+    }
+}
+
+class Game extends React.Component {
+    render() {
+
+    }
+}
+
+// ------------------------------------
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
+
+
+
