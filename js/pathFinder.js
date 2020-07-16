a32ed13d308275fdd67373ae96784ae4363025ef
vtittert <vtittert@ucsc.edu>
2020-07-15 21:52:57 -0700

    add an array to represent the grid

--- js/pathFinder.js
+++ js/pathFinder.js
@@ -3,8 +3,19 @@ alert("Testing Javascript");
 var c = document.getElementById("mapContainer");
 var ctx = c.getContext("2d");
 
-// draw grid
 var i;
+var j;
+
+// setup grid
+var grid = [];
+for (i = 0; i < 20; i++) {
+    grid[i] = [];
+    for (j = 0; j < 10; j++) {
+        grid[i][j] = 0;
+    }
+}
+
+// draw grid
 for (i = 1; i < 20; i++) {
     ctx.moveTo(50 * i, 0);
     ctx.lineTo(50 * i, 500);
@@ -14,3 +25,15 @@ for (i = 1; i < 10; i++) {
     ctx.lineTo(1000, 50 * i);
 }
 ctx.stroke();
+
+/*
+// test grid array
+for (i = 0; i < 20; i++) {
+    for (j = 0; j < 10; j++) {
+        if (grid[i][j] === 0) {
+            ctx.fillText("0", 25 + 50 * i, 25 + 50 * j);
+        }
+    }
+}
+*/
+
