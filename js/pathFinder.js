7506ca6fee0fb9539c624fc4e58f843436fb38e1
vtittert <vtittert@ucsc.edu>
2020-07-15 20:51:39 -0700

    draw a grid on the canvas

--- js/pathFinder.js
+++ js/pathFinder.js
@@ -1,2 +1,16 @@
 alert("Testing Javascript");
 
+var c = document.getElementById("mapContainer");
+var ctx = c.getContext("2d");
+
+// draw grid
+var i;
+for (i = 1; i < 20; i++) {
+    ctx.moveTo(50 * i, 0);
+    ctx.lineTo(50 * i, 500);
+}
+for (i = 1; i < 10; i++) {
+    ctx.moveTo(0, 50 * i);
+    ctx.lineTo(1000, 50 * i);
+}
+ctx.stroke();
