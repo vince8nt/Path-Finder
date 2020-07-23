97b32f2cd1938c4d9ffd6f74921cde2afde3b57f
vtittert <vtittert@ucsc.edu>
2020-07-22 17:42:00 -0700

    add dijkstra's alg for pathfinding

--- js/pathFinder.js
+++ js/pathFinder.js
@@ -206,7 +206,7 @@ function go () {
     else {
         goStatus = "going";
         drawGo();
-        BFS();
+        Dijkstra();
         goStatus = "clear";
         drawGo();
     }
@@ -284,6 +284,85 @@ function BFS () {
         }
     }
     alert("No path was found.");
+}
+
+function Dijkstra() {
+    var searchGrid = [];
+    for (i = 0; i < 20; i++) {
+        searchGrid[i] = [];
+        for (j = 0; j < 10; j++) {
+            searchGrid[i][j] = [[-1, -1], -1, true];
+        }
+    }
+
+    searchGrid[startX][startY][1] = 0; // set depth to 0
+    ctx.strokeStyle = "#9090FF";
+    ctx.lineWidth = 2;
+
+    while (true) {
+        // find shortest move
+        var x = -1,
+            y = -1;
+        var min = -1;
+        for (i = 0; i < 20; i++) {
+            for (j = 0; j < 10; j++) {
+                if (searchGrid[i][j][2]) {
+                    var temp = searchGrid[i][j][1];
+                    if (temp !== -1) {
+                        if (min === -1) {
+                            min = temp;
+                            x = i;
+                            y = j;
+                        }
+                        else {
+                            if (temp < min) {
+                                min = temp;
+                                x = i;
+                                y = j;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // no move found
+        if (min === -1) {
+            alert("No path was found.");
+            return;
+        }
+        // remove tile
+        searchGrid[x][y][2] = false;
+        // path found
+        if (x === endX && y === endY) {
+            alert("A " + searchGrid[x][y][1] + " length path was found.");
+            traceBack(searchGrid);
+            return;
+        }
+        // try all neighbors
+        neighbor(x - 1, y, x, y, searchGrid);
+        neighbor(x + 1, y, x, y, searchGrid);
+        neighbor(x, y - 1, x, y, searchGrid);
+        neighbor(x, y + 1, x, y, searchGrid);
+        neighbor(x - 1, y - 1, x, y, searchGrid);
+        neighbor(x - 1, y + 1, x, y, searchGrid);
+        neighbor(x + 1, y - 1, x, y, searchGrid);
+        neighbor(x + 1, y + 1, x, y, searchGrid);
+    }
+}
+
+function neighbor(x, y, orX, orY, searchGrid) {
+    if (-1 < x && -1 < y && 20 > x && 10 > y && grid[x][y] === 0) {
+        var dist = searchGrid[orX][orY][1];
+        dist += Math.sqrt(Math.abs(x - orX) + Math.abs(y - orY));
+        if (searchGrid[x][y][1] === -1 || dist < searchGrid[x][y][1]) {
+            searchGrid[x][y][1] = dist;
+            searchGrid[x][y][0] = [orX, orY];
+            ctx.beginPath();
+            ctx.moveTo(50 * orX + 25, 50 * orY + 25);
+            ctx.lineTo(50 * x + 25, 50 * y + 25);
+            ctx.stroke();
+        }
+    }
 }
 
 function traceBack(searchGrid) {
